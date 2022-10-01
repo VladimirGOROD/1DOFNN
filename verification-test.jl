9eb7438f0a93a820aa17b58de789765a2e1fc9a3
Vladimir <volodoc@mail.ru>
2022-10-01 17:46:54 +0300

    work

--- verification-test.jl
+++ verification-test.jl
@@ -5,11 +5,11 @@ end
 using Jurvis;       
 using PyPlot;
 using LinearAlgebra
-
-t = (0.0:1e-4:5.0); # time frames
+dt = 1e-4;
+t = (0.0:dt:5.0); # time frames
 p=[-0.0030, -0.02375, 3.775, 0.5, 50, 100, 80 ];
-e=2.7182;
 damp1(t) = 0.002 *(2- (t/30)^2); 
+
 ζ = damp1.(t);
 damp2(t) = 0.008;
 ζ2 = damp2.(t);
@@ -19,17 +19,21 @@ freq2(t)=3*exp((-t*1.5)-p[4])+87;
 g2 = freq2.(t);
 # plot(t,g2);
 ω1 = 2*π*g;
+ϕ1 = cumsum(ω1) * dt;
 ω2 = 2*π*g2;
-A1 = p[6]*exp.(-ζ.*ω1.*t);
-A2 =p[7].*e.^(-ζ2.*ω2.*t);
-θ1 = A1.*sin.(ω1.*t);
-θ2=A2.*sin.(ω2.*t.+π/8);
+ϕ2 = cumsum(ω2) * dt;
+A1 = p[6]*exp.(-ζ.*ϕ1);
+A2 =p[7].*e.^(-ζ2.*ϕ2);
+θ1 = A1.*sin.(ϕ1);
+θ2=A2.*sin.(ϕ2.+π/8);
 θ=θ1+θ2;
+plot(θ);
+
 
 ##
 
 SSA_win = 2000;
-signal = MeasuredData("test data", t, hcat(θ, θ1, θ2), "time", ["test signal", "mode1", "mode2"], size(θ,1), 3, ["", "", ""])
+signal = MeasuredData("test data", t, hcat(θ, θ1, θ2), "time", ["test signal", "mode1", "mode2"], size(θ,1), 3, ["", "", ""]);
 decomposed_data = decomposeSSA(signal,1, 10, SSA_win);
 plotall(spectrumall(decomposed_data)); yscale("log"); legend();xlim([0,200])
 plotall(decomposed_data)
@@ -67,7 +71,7 @@ env_modes1 = decomposeSSA(env1, 10, 1000);
 add_data!(mode1, env_modes1[:,1]; ydata_name = "Огибающая первой моды"); #канал 2
 phase1 = instphase(mode1.ydata[:,1]);
 add_data!(mode1, phase1; ydata_name = "inst phase моды 1"); #канал 3
-
+# plot(mode1.ydata[:,1])
 inst_freq1 = findiff(mode1.ydata[:,3], t[2] - t[1]; order = 4) / (2*π);
 freq_modes1 = decomposeSSA(inst_freq1, 10,1000);
 plot(inst_freq1);plot(freq_modes1[:,1]); plot(g)
@@ -121,10 +125,67 @@ ylim([0.0038,0.004]);
 figure()
 plot(t,ζ2)
 plotchannel(mode2;ch=4)
+ylim([0,0.01])
 end
 
 
 figure()
-plot(t,ζ)
+plot(t,ζ1)
 plotchannel(mode1;ch=4)
 ylim([0.0038,0.004]);
+
+
+X=Vector{Float64}([])#вектор для точек времени перехода через 0
+begin
+  for i=1:(mode1.length-1)
+    if mode1.ydata[i,1] < 0 && mode1.ydata[i+1,1] >0
+      y=0;
+      y_a=mode1.ydata[i,1];
+      y_b=mode1.ydata[i+1,1];
+      x_a=t[i];
+      x_b=t[i+1];
+      x_0=((y-y_a)/(y_b-y_a))*(x_b-x_a)+x_a;
+      push!(X,x_0);
+
+    end
+
+  end
+end
+
+function calcfreq(sig::AbstractVector{<:Number}, time::AbstractVector{<:Number})
+  X=Vector{Float64}([]);
+  for i in 1: size(sig,1)-1
+    if sig[i] < 0 && sig[i+1] >0
+      y=0;
+      y_a=sig[i];
+      y_b=sig[i+1];
+      x_a=time[i];
+      x_b=time[i+1];
+      x_0=((y-y_a)/(y_b-y_a))*(x_b-x_a)+x_a;
+      push!(X,x_0);
+    end
+  end
+  return X
+end
+
+t1 = calcfreq(θ1, t)
+f1 = 1 ./findiff(t1,1; order = 4)
+instfreq = 1 ./ findiff(X,1; order=4)
+
+par = LinRange(0,1,size(t,1))
+gg = 51 .* (1 .- par) + 40 .* (par)
+ω = g*2*pi;
+ϕ = cumsum(ω) * dt
+
+testsig = sin.(ϕ);
+t2 = calcfreq(testsig, t)
+f2 = 1 ./ findiff(t2, 1; order = 1)
+# plot(testsig)
+# testsig2 = sin.(2*pi*51 .*t)
+# t2 = calcfreq(testsig2, t)
+# f2 = 1 ./ findiff(t2, 1; order = 4)
+
+plot(t,g); plot(X,instfreq); plot(t1,f1); plot(t2  ,f2, "--"); ylim([49,52])
+
+figure();plot(testsig)
+plot(t,testsig)
